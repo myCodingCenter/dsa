9e11ed4dc239464a75b4157d0f99331a5b001ac6
Gemmy <cccontactme1@gmail.com>
2024-08-25 07:38:29 +0545

    add struct

---
+++ 00_struct/03_pointer_struct.c
@@ -0,0 +1,24 @@
+/* Passing pointers to structures as arguments
+
+// If the size of the structure is very large then passing the copy of the whole structure is not efficient.
+// BETTER CHOICE : Pass the address of the structure.
+
+*/
+
+#include <stdio.h>
+struct point
+{
+    int x;
+    int y;
+};
+
+void print(struct point *ptr){
+    printf("%d %d\n",ptr->x,ptr->y);
+}
+#include <stdio.h>
+void main(){
+    struct point p1 = {2,3};
+    struct point p2 = {3,2};
+    print(&p1);
+    print(&p2);
+}
